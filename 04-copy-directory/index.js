91307ab32cc4fae7bb09a16aab792e8b26ec6507
Aleksandr <alex-davydov211@mail.ru>
2022-09-21 22:01:14 +0300

    update: index.js 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyDir = () => {
+    try {
+        fsPromises.mkdir(__dirname, { recursive: true })
+    } catch (err) {
+        console.error(err.message);
+      }
+}
