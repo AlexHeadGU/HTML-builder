c04c8d30b90938b31febd36c08404ddd8d1875c6
Aleksandr <alex-davydov211@mail.ru>
2022-09-21 10:05:05 +0300

    feat: solution 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path')
 
-
 const dirPath = path.join(__dirname, '/secret-folder/')
 
 fs.readdir(dirPath, (err, files) => {
@@ -10,26 +9,17 @@ fs.readdir(dirPath, (err, files) => {
     else {
       console.log("\nCurrent directory filenames:");
       files.forEach(file => {
-
-        console.log( typeof file);
-
         fs.stat(dirPath + file, (err, stats) => {
             if (err) throw err;
-          
-            // console.log(`stats: ${JSON.stringify(stats)}`);
+            const str = path.parse(file).ext;
+            const newExt = str.replace(/./, '');
+
             if (stats.isFile) {
-                console.log(`${path.parse(file).name} - ${path.parse(file).ext} - ${stats.size}`);
+                console.log(`${path.parse(file).name} - ${newExt} - ${stats.size/8}kb`);
             } else {
                 console.log("Not a file!");
             }
         });
-
-
-        // console.log(fs.stat())
-        // console.log(file.isFile)
-        // if (file.isFile) {
-        //     console.log(file);
-        // }
       })
     }
   })
