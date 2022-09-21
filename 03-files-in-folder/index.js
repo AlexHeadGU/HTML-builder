5707426c7a86b6736704781c3e3fcb74073eb5b3
Aleksandr <alex-davydov211@mail.ru>
2022-09-21 10:07:38 +0300

    refactor: delete spaces

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,22 +4,22 @@ const path = require('path')
 const dirPath = path.join(__dirname, '/secret-folder/')
 
 fs.readdir(dirPath, (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      console.log("\nCurrent directory filenames:");
-      files.forEach(file => {
-        fs.stat(dirPath + file, (err, stats) => {
-            if (err) throw err;
-            const str = path.parse(file).ext;
-            const newExt = str.replace(/./, '');
+  if (err)
+    console.log(err);
+  else {
+    console.log("\nCurrent directory filenames:");
+    files.forEach(file => {
+      fs.stat(dirPath + file, (err, stats) => {
+        if (err) throw err;
+        const str = path.parse(file).ext;
+        const newExt = str.replace(/./, '');
 
-            if (stats.isFile) {
-                console.log(`${path.parse(file).name} - ${newExt} - ${stats.size/8}kb`);
-            } else {
-                console.log("Not a file!");
-            }
-        });
-      })
-    }
-  })
+        if (stats.isFile) {
+          console.log(`${path.parse(file).name} - ${newExt} - ${stats.size/8}kb`);
+        } else {
+          console.log("Not a file!");
+        }
+      });
+    })
+  }
+})
