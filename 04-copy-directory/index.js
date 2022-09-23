fcc155bf645b9c8707f6ab3fd1114d73eddd8197
Aleksandr <alex-davydov211@mail.ru>
2022-09-23 11:07:57 +0300

    feat: solution 4rd task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
+const pathNewDir = path.join(__dirname, 'files-copy/');
+const pathDirForCopy = path.join(__dirname, 'files/')
+
+console.log(pathDirForCopy);
 const copyDir = () => {
-    try {
-        fsPromises.mkdir(__dirname, { recursive: true })
-    } catch (err) {
-        console.error(err.message);
-      }
+    fs.mkdir(pathNewDir, { recursive: true }, (err) => {
+        if(err) throw err; 
+        console.log('Папка успешно создана');
+    });
+
+    fs.readdir(pathDirForCopy, (err, files) => {
+        if (err) {
+            console.log(err);
+        }else {
+            files.forEach(file => {
+                fs.copyFile(pathDirForCopy + file, pathNewDir + file, (err) => {
+                    if(err) throw err; 
+                    console.log('Файл успешно скопирован');
+                });
+            });
+        }
+    })
 }
+
+copyDir();
