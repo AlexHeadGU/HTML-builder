65fa955ac189cb201ff37b530715d899608b5144
Aleksandr <alex-davydov211@mail.ru>
2022-10-11 15:55:50 +0300

    feat: solution 6th task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,12 +1,16 @@
-const { dir } = require('console');
 const fs = require('fs');
 const path = require('path');
 
 const pathDist = path.join(__dirname + '/project-dist');
+const templatePath = path.join(__dirname + '/template.html');
 const styleCssPath = path.join(__dirname + '/project-dist/style.css');
 const styleDirPath = path.join(__dirname + '/styles/');
 const assetsDir = path.join(__dirname + '/assets/');
 const distAssetsDir = path.join(pathDist + '/assets/');
+const indexPath = path.join(__dirname, 'project-dist', 'index.html');
+
+let arrHtml = [];
+let addDataHtml = [];
 
 const makeDir = () => {
     fs.mkdir(pathDist, { recursive: true }, (err) => {
@@ -87,6 +91,51 @@ const copyDir = (src, dist) => {
     })
 }
 
+const createHtml = () => {
+    fs.copyFile(templatePath, pathDist + '/index.html', (error) => {
+        if (error) throw error;
+    })
+}
+
+const insertHtmlData = () => {
+    fs.copyFile(templatePath, indexPath, (err) => {
+        if (err) {
+        return console.error(err);
+        }
+    });
+  
+    fs.readFile(templatePath, 'utf-8', (err, data) => {
+        if (err) console.log(err);
+    
+        let templateData = data;
+        const templateTags = data.match(/{{\w+}}/gm);
+    
+        for (let tag of templateTags) {
+            const tagPath = path.join(
+                __dirname,
+                '/components',
+                `${tag.slice(2, -2)}.html`,
+            );
+        
+            fs.readFile(tagPath, 'utf-8', (err, dataTag) => {
+                if (err) console.log(err);
+        
+                templateData = templateData.replace(tag, dataTag);
+        
+                fs.rm(indexPath, { recursive: true, force: true }, (err) => {
+                if (err) {
+                    return console.error(err);
+                }
+                const index = fs.createWriteStream(indexPath);
+                index.write(templateData);
+                });
+            });
+        }
+    });
+}
+
 makeDir();
 isExistStyle();
 copyDir(assetsDir, distAssetsDir);
+createHtml();
+insertHtmlData();
